ae14215e68723155d2fc2cd6d0afbc0982b9d879
Jonathan Reem <jonathan.reem@gmail.com>
2015-03-13 01:12:28 -0700

    (refactor) Completely refactor Try to eliminate TryHandler.
    Also add some documentation to the source, and #[inline]
    annotations.

--- src/lib.rs
+++ src/lib.rs
@@ -3,121 +3,102 @@
 
 //! # iron-try
 //!
-//! A handler for chaining sub-handlers.
+//! A handler for trying handlers in succession.
+//!
+//! For any tuple of handlers used in `Try`, `Try`s `Handler`
+//! implementation will call each handler's `handle` method until
+//! one returns an error-free `Response`.
+//!
+//! The same procedure is done for `Try<Vec<Box<Handler>>>`.
 //!
 
 extern crate iron;
 
-pub struct Try<H: tryhandler::TryHandler>(H);
+use iron::prelude::*;
+use iron::Handler;
 
-mod tryhandler {
-    use iron::Handler;
-    use iron::prelude::*;
+/// A handler for trying handlers in succession.
+///
+/// For any tuple of handlers used in `Try`, `Try`s `Handler`
+/// implementation will call each handler's `handle` method until
+/// one returns an error-free `Response`.
+///
+/// The same procedure is done for `Try<Vec<Box<Handler>>>`. Keep
+/// in mind the `Vec` must not be empty.
+pub struct Try<H>(H);
 
-    use std::iter::IntoIterator;
-
-    pub trait TryHandler: Send + Sync {
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response>;
+impl<H: Handler> Handler for Try<H> {
+    #[inline]
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        self.0.handle(req)
     }
+}
 
-    impl<'a, I: IntoIterator<Item=&'a Handler> + Send + Sync> TryHandler for I {
-        /// ## Panics
-        ///
-        /// Panics if self yields an empty iterator.
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response> {
-            let mut iter = self.into_iter();
-            let first = iter.next()
-                .expect("An iterator with at least on element.")
-                .handle(req);
+impl Handler for Try<Vec<Box<Handler>>> {
+    #[inline]
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        let mut iter = self.0.iter();
+        let first = iter.next()
+            .expect("A non-empty Vec of Handlers for Try.")
+            .handle(req);
 
-            iter.fold(first, |a, n| a.or_else(|_| n.handle(req)))
-        }
+        iter.fold(first, |a, n| a.or_else(|_| n.handle(req)))
     }
+}
 
-    impl<H: Handler> TryHandler for H {
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response> {
-            self.handle(req)
-        }
+impl<H1: Handler, H2: Handler> Handler for Try<(H1, H2)> {
+    #[inline]
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        (self.0).0.handle(req)
+            .or_else(|_| (self.0).1.handle(req))
     }
+}
 
-    impl<H1: Handler, H2: Handler> TryHandler for (H1, H2) {
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response> {
-            self.0.handle(req)
-                .or_else(move |_| self.1.handle(req))
-        }
+impl<H1, H2, H3> Handler for Try<(H1, H2, H3)>
+where H1: Handler, H2: Handler, H3: Handler {
+    #[inline]
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        (self.0).0.handle(req)
+            .or_else(|_| (self.0).1.handle(req))
+            .or_else(|_| (self.0).2.handle(req))
     }
+}
 
-    impl<H1, H2, H3> TryHandler for (H1, H2, H3)
-    where H1: Handler, H2: Handler, H3: Handler {
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response> {
-            self.0.handle(req)
-                .or_else(|_| self.1.handle(req))
-                .or_else(|_| self.2.handle(req))
-        }
+impl<H1, H2, H3, H4> Handler for Try<(H1, H2, H3, H4)>
+where H1: Handler, H2: Handler, H3: Handler, H4: Handler {
+    #[inline]
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        (self.0).0.handle(req)
+            .or_else(|_| (self.0).1.handle(req))
+            .or_else(|_| (self.0).2.handle(req))
+            .or_else(|_| (self.0).3.handle(req))
     }
+}
 
-    impl<H1, H2, H3, H4> TryHandler for (H1, H2, H3, H4)
-    where H1: Handler, H2: Handler, H3: Handler, H4: Handler {
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response> {
-            self.0.handle(req)
-                .or_else(|_| self.1.handle(req))
-                .or_else(|_| self.2.handle(req))
-                .or_else(|_| self.3.handle(req))
-        }
+impl<H1, H2, H3, H4, H5> Handler for Try<(H1, H2, H3, H4, H5)>
+where H1: Handler, H2: Handler, H3: Handler, H4: Handler,
+      H5: Handler {
+    #[inline]
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        (self.0).0.handle(req)
+            .or_else(|_| (self.0).1.handle(req))
+            .or_else(|_| (self.0).2.handle(req))
+            .or_else(|_| (self.0).3.handle(req))
+            .or_else(|_| (self.0).4.handle(req))
     }
+}
 
-    impl<H1, H2, H3, H4, H5> TryHandler for (H1, H2, H3, H4, H5)
-    where H1: Handler, H2: Handler, H3: Handler, H4: Handler, H5: Handler {
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response> {
-            self.0.handle(req)
-                .or_else(|_| self.1.handle(req))
-                .or_else(|_| self.2.handle(req))
-                .or_else(|_| self.3.handle(req))
-                .or_else(|_| self.4.handle(req))
-        }
-    }
-
-    impl<H1, H2, H3, H4, H5, H6> TryHandler for (H1, H2, H3, H4, H5, H6)
-    where H1: Handler, H2: Handler, H3: Handler, H4: Handler,
-          H5: Handler, H6: Handler {
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response> {
-            self.0.handle(req)
-                .or_else(|_| self.1.handle(req))
-                .or_else(|_| self.2.handle(req))
-                .or_else(|_| self.3.handle(req))
-                .or_else(|_| self.4.handle(req))
-                .or_else(|_| self.5.handle(req))
-        }
-    }
-
-    impl<H1, H2, H3, H4, H5, H6, H7> TryHandler for (H1, H2, H3, H4, H5, H6, H7)
-    where H1: Handler, H2: Handler, H3: Handler, H4: Handler,
-          H5: Handler, H6: Handler, H7: Handler {
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response> {
-            self.0.handle(req)
-                .or_else(|_| self.1.handle(req))
-                .or_else(|_| self.2.handle(req))
-                .or_else(|_| self.3.handle(req))
-                .or_else(|_| self.4.handle(req))
-                .or_else(|_| self.5.handle(req))
-                .or_else(|_| self.6.handle(req))
-        }
-    }
-
-    impl<H1, H2, H3, H4, H5, H6, H7, H8>
-    TryHandler for (H1, H2, H3, H4, H5, H6, H7, H8)
-    where H1: Handler, H2: Handler, H3: Handler, H4: Handler,
-          H5: Handler, H6: Handler, H7: Handler, H8: Handler {
-        fn try_handle(&self, req: &mut Request) -> IronResult<Response> {
-            self.0.handle(req)
-                .or_else(|_| self.1.handle(req))
-                .or_else(|_| self.2.handle(req))
-                .or_else(|_| self.3.handle(req))
-                .or_else(|_| self.4.handle(req))
-                .or_else(|_| self.5.handle(req))
-                .or_else(|_| self.6.handle(req))
-                .or_else(|_| self.7.handle(req))
-        }
+impl<H1, H2, H3, H4, H5, H6> Handler for Try<(H1, H2, H3, H4, H5, H6)>
+where H1: Handler, H2: Handler, H3: Handler, H4: Handler,
+      H5: Handler, H6: Handler {
+    #[inline]
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        (self.0).0.handle(req)
+            .or_else(|_| (self.0).1.handle(req))
+            .or_else(|_| (self.0).2.handle(req))
+            .or_else(|_| (self.0).3.handle(req))
+            .or_else(|_| (self.0).4.handle(req))
+            .or_else(|_| (self.0).5.handle(req))
     }
 }
 
